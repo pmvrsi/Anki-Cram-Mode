0fe9934fcfe66a8a85ffc4f470c4480bbb6b46ff
paramveersi <y5i31@students.keele.ac.uk>
2026-02-03 10:02:45 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,53 +3,19 @@ from aqt.qt import *
 from aqt.utils import showInfo, tooltip
 
 CRAM_DECK_NAME = "Cram Mode"
-TEXT_START = "⚡ Cram"     
-TEXT_STOP = "🛑 Stop"
 
 class CramModeAddon:
     def __init__(self):
-        self.action = None
-        self.toolbar = None
         self.setup_ui()
-        
-        mw.addonManager.setConfigAction(__name__, self.show_config)
-        from aqt.gui_hooks import state_did_reset
-        state_did_reset.append(self.update_ui_state)
 
     def setup_ui(self):
-        self.action = QAction(TEXT_START, mw)
-        self.action.setShortcut("Ctrl+Shift+C")
-        self.action.triggered.connect(self.toggle_cram)
-        
-        mw.form.menuTools.addAction(self.action)
-        
-        if hasattr(mw.form, 'mainToolBar'):
-            mw.form.mainToolBar.addAction(self.action)
-        else:
-            self.toolbar = QToolBar("Cram Mode")
-            self.toolbar.setObjectName("CramModeToolbar")
-            self.toolbar.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
-            self.toolbar.addAction(self.action)
-            mw.addToolBar(Qt.ToolBarArea.TopToolBarArea, self.toolbar)
+        self.action_start = QAction("Start Cram Mode", mw)
+        self.action_start.triggered.connect(self.start_cram_mode)
+        mw.form.menuTools.addAction(self.action_start)
 
-    def is_cramming(self):
-        if not mw.col: return False
-        return bool(mw.col.decks.id(CRAM_DECK_NAME))
-
-    def update_ui_state(self):
-        if self.is_cramming():
-            self.action.setText(TEXT_STOP)
-            self.action.setToolTip("Click to Restore Original Decks")
-        else:
-            self.action.setText(TEXT_START)
-            self.action.setToolTip("Click to Create Filtered Cram Deck")
-
-    def toggle_cram(self):
-        if self.is_cramming():
-            self.stop_cram_mode()
-        else:
-            self.start_cram_mode()
-        self.update_ui_state()
+        self.action_stop = QAction("Stop Cram Mode", mw)
+        self.action_stop.triggered.connect(self.stop_cram_mode)
+        mw.form.menuTools.addAction(self.action_stop)
 
     def start_cram_mode(self):
         current_deck = mw.col.decks.current()
@@ -89,8 +55,7 @@ class CramModeAddon:
                 tooltip("✅ Decks Restored", period=1500)
             except:
                 showInfo("Cram Mode OFF")
-
-    def show_config(self):
-        showInfo("No configuration needed.")
+        else:
+            tooltip("Cram Mode is not active", period=1500)
 
 cram_mode = CramModeAddon()
